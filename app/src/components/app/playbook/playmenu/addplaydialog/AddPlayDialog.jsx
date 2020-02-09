0e2163028a8326b4683d813eb6c3714c3d4db5ce
mshah87 <mauliksc87@outlook.com>
2020-02-08 19:45:23 -0500

    created import functionality

--- app/src/components/app/playbook/playmenu/addplaydialog/AddPlayDialog.jsx
+++ app/src/components/app/playbook/playmenu/addplaydialog/AddPlayDialog.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Modal, Input } from 'antd';
+import { Modal } from 'antd';
+import { post } from 'axios';
 import PropTypes from 'prop-types';
 
 class AddPlayDialog extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      playName: '',
-    };
+    this.state ={
+      file:null
+    }
+    this.onFormSubmit = this.onFormSubmit.bind(this)
+    this.onChange = this.onChange.bind(this)
+    this.fileUpload = this.fileUpload.bind(this)
   }
 
     /**
@@ -38,6 +42,27 @@ class AddPlayDialog extends React.Component {
       });
     }
 
+    onFormSubmit(e){
+      e.preventDefault() // Stop form submit
+      this.fileUpload(this.state.file).then((response)=>{
+        console.log(response.data);
+      })
+    }
+    onChange(e) {
+      this.setState({file:e.target.files[0]})
+    }
+    fileUpload(file){
+      const url = '';
+      const formData = new FormData();
+      formData.append('file',file)
+      const config = {
+          headers: {
+              'content-type': 'multipart/form-data'
+          }
+      }
+      return  post(url, formData,config)
+    }
+
     render() {
       const { addDialogVisible } = this.props;
       return (
@@ -45,10 +70,16 @@ class AddPlayDialog extends React.Component {
           title="Import a file"
           visible={addDialogVisible}
           onOk={this.handleOk}
-          onCancel={this.handleCancel}
-        >
-          <Input id="add-play" placeholder="Play name" onChange={this.handleChange} onPressEnter={this.handleOk} />
-        </Modal>
+          onCancel={this.handleCancel}>
+
+
+      <form onSubmit={this.onFormSubmit}>
+        <h1>File Upload</h1>
+        <input type="file" onChange={this.onChange} />
+        <button type="submit">Upload</button>
+      </form>    
+
+      </Modal>
       );
     }
 }
